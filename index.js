d29f456e549d3c048c5fc859797c341a1e7da912
Mona Lisa <david.t.bui83@gmail.com>
2020-06-13 18:25:41 -0700

    building template

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 //////////////////////////////////////////
 // FILE
@@ -28,9 +29,28 @@ const http = require('http');
 
 //////////////////////////////////////////
 // SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!');
+  const pathName = req.url;
+
+  if(pathName === '/' || pathName === '/overview'){
+    res.end('This is the OVERVIEW');
+  }else if (pathName === '/product'){
+    res.end('This is the PRODUCT')
+  }else if (pathName === '/api'){
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+    res.end(data);
+  }else{
+    res.writeHead(404, {
+      'Content-Type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 const PORT = 8000
