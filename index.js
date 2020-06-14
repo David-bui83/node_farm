b315d64b346bbe398020bc175104ea1484156480
Mona Lisa <david.t.bui83@gmail.com>
2020-06-13 19:32:25 -0700

    added modules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modueles/replaceTemplate');
 
 //////////////////////////////////////////
 // FILE
@@ -29,21 +30,46 @@ const url = require('url');
 
 //////////////////////////////////////////
 // SERVER
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
 
-  if(pathName === '/' || pathName === '/overview'){
-    res.end('This is the OVERVIEW');
-  }else if (pathName === '/product'){
-    res.end('This is the PRODUCT')
-  }else if (pathName === '/api'){
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if(pathname === '/' || pathname === '/overview'){
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
+  
+  // Product page
+  }else if (pathname === '/product'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+  
+  // API
+  }else if (pathname === '/api'){
     res.writeHead(200, {
       'Content-Type': 'application/json'
     })
     res.end(data);
+  
+  // Not Found
   }else{
     res.writeHead(404, {
       'Content-Type': 'text/html',
